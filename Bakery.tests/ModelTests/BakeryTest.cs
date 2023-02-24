a343d14c5cf31a11782a7e3df61bee66163c61bb
Jannon Sielaff <jannon.sielaff@gmail.com>
2023-02-24 12:59:37 -0800

    add basic testing skeleton

---
+++ Bakery.tests/ModelTests/BakeryTest.cs
@@ -0,0 +1,24 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bread.Models;
+using Pastry.Models;
+using System.Collections.Generic;
+using System;
+
+namespace Bakery.Tests
+{
+  [TestClass]
+  public class BakeryTests
+  {
+    [TestMethod]
+        public void FinallyTheNameOfTheTest()
+    {
+
+    }
+    //public void NameOfMethod_WhatTheTestDoes_DesiredOutcome()
+    //arrange - get all your variables
+    //act - do what you're gonna do with the variables
+    //assert - make sure it does what you want it to
+
+  }
+
+}
