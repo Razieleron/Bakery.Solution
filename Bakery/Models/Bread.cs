3d3a1428bb36be24ca2c81aa854c63b16beb8d4a
Jannon Sielaff <jannon.sielaff@gmail.com>
2023-02-24 13:05:41 -0800

    testing folder structure

---
+++ Bakery/Models/Bread.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+
+namespace Products.Models
+{
+  public class Bread
+  {
+
+    public static int calculateBreadCost(int input)
+    {
+      
+      // Dictionary<char, int> scores = new Dictionary<char, int>()
+      // {
+      //   {'a', 1}, {'e', 1}, {'i', 1}, {'o', 1}, {'u', 1}, {'l', 1}, {'n', 1}, {'r', 1}, {'s', 1}, {'t', 1},
+      //   {'d', 2}, {'g', 2},
+      //   {'b', 3}, {'c', 3}, {'m', 3}, {'p', 3},
+      //   {'f', 4}, {'h', 4}, {'v', 4}, {'w', 4}, {'y', 4},
+      //   {'k', 5},
+      //   {'j', 8}, {'x', 8},
+      //   {'q', 10}, {'z', 10}
+      // };
+      // int wordScore = 0;
+      // foreach (char letter in input)
+      // {
+      //   if (scores.ContainsKey(letter))
+      //   {
+      //     wordScore += scores[letter];
+      //   }
+      // }
+      // return wordScore;
+    }
+  }
+}
---
+++ Bakery/Program.cs
@@ -0,0 +1,16 @@
+using System;
+using Products.Models;
+
+namespace Bakery
+{
+  class Program
+  {
+    static void Main()
+    {
+      // ScrabbleScore scrabbler = new ScrabbleScore();
+      // Console.WriteLine("Hey there gimme a word");
+      // string input = Console.ReadLine().ToLower();
+      // Console.WriteLine(ScrabbleScore.callScore(input)); 
+    }
+  }
+}
